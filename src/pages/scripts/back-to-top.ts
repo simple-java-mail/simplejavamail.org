7b894d0ea16d9945be510a8150ba3c00268048b4
bbottema <b.bottema@projectnibble.org>
2019-12-26 14:22:13 +0100

    Added universal back-top-top link to all headers

---
+++ src/pages/scripts/back-to-top.ts
@@ -0,0 +1,13 @@
+const headers = document.querySelectorAll('.section-link + h2, .section-link + h3');
+
+headers.forEach(header => {
+	const space = document.createElement('span');
+	space.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;';
+	header.appendChild(space);
+
+	const anchor = document.createElement('a');
+	anchor.innerText = 'back';
+	anchor.className = 'link_top';
+	anchor.href = '#navigation';
+	header.appendChild(anchor);
+});
