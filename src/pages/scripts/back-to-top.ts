18d4d42821c7c3d063364e3a8f96f64bb445bd50
bbottema <b.bottema@projectnibble.org>
2019-12-27 12:51:36 +0100

    moved the back button a little bit closer to its header

--- src/pages/scripts/back-to-top.ts
+++ src/pages/scripts/back-to-top.ts
@@ -1,7 +1,7 @@
 document.querySelectorAll('.section-link + h2, .section-link + h3')
 	.forEach(header => {
 		const space = document.createElement('span');
-		space.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;';
+		space.innerHTML = '&nbsp;&nbsp;&nbsp;';
 		header.appendChild(space);
 
 		const anchor = document.createElement('a');
