2ba25f5221ae05f11ff57673f0c43a12d7f29df7
bbottema <b.bottema@projectnibble.org>
2019-12-27 11:40:49 +0100

    Lazy loading images from webp assets if supported

--- src/pages/scripts/lazy-img-loader.ts
+++ src/pages/scripts/lazy-img-loader.ts
@@ -1,4 +1,22 @@
-window.addEventListener('load', () => {
-	document.querySelectorAll('img[data-src]')
-		.forEach(img => img.setAttribute('src', img.getAttribute('data-src')));
-}, false);
+async function supportsWebp():Promise<boolean> {
+	const img = new Image();
+	return new Promise(resolve => {
+		img.onload = function () {resolve((img.width > 0) && (img.height > 0));};
+		img.onerror = function () {resolve(false);};
+		img.src = "data:image/webp;base64," + "UklGRiIAAABXRUJQVlA4IBYAAAAwAQCdASoBAAEADsD+JaQAA3AAAAAA";
+	});
+}
+
+(async () => {
+	const supportsWebpKnownPromise = supportsWebp();
+	window.addEventListener('load', () => {
+		supportsWebpKnownPromise.then(supportsWebpKnown => {
+			// console.debug('document loaded, webp supported: ' + supportsWebpKnown);
+			document.querySelectorAll('img[data-src]')
+				.forEach(img => img.setAttribute('src',
+					img.hasAttribute('data-src-webp') && supportsWebpKnown
+						? img.getAttribute('data-src-webp')
+						: img.getAttribute('data-src')));
+		});
+	}, false);
+})();
