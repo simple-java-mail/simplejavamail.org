719a98559b1951a509d0da99f8eba9bf1a1ed16b
bbottema <b.bottema@projectnibble.org>
2020-04-26 08:32:06 +0200

    Upgraded to Gulp 4 to be compatible with Node 12

--- gulpfile.js
+++ gulpfile.js
@@ -13,53 +13,22 @@ const ts = require('gulp-typescript');
 const config = {
   src: './src',
   dest: './dist',
-  watchers: [{
-    match: ['./src/**/*.hbs'],
-    tasks: ['html']
-  }, {
-    match: ['./src/**/*.less'],
-    tasks: ['less']
-  }, {
-    match: ['./src/**/*.ts', './tsconfig.json'],
-    tasks: ['ts']
-  }, {
-    match: ['./src/assets/**/*'],
-    tasks: ['copy-assets']
-  }, {
-    match: ['./gulpfile.js'],
-    tasks: ['html', 'less', 'ts', 'copy-assets', 'copy-lib', 'copy-sitemap']
-  }]
 };
 
-gulp.task('clean', () => del.sync(config.dest));
-
-gulp.task('build', ['clean', 'html', 'less', 'ts', 'copy-assets', 'copy-lib', 'copy-sitemap'], () => {
-  // noinspection JSUnusedGlobalSymbols
-  return gulp.src(`${config.src}/pages/*.hbs`)
-      .pipe(handlebars({}, {
-        ignorePartials: true,
-        batch: [`${config.src}/partials`],
-        helpers : {
-          valueWhenEqual : function(valA, valB, returnVal){
-            return valA === valB ? returnVal : '';
-          }
-        }
-      }))
-      .pipe(rename({ extname: '.html' }))
-      .pipe(gulp.dest(config.dest));
+gulp.task('clean', done => {
+	del.sync(config.dest);
+	done();
 });
 
-gulp.task('html', [], () => {
+gulp.task('html', () => {
   // noinspection JSUnusedGlobalSymbols
   return gulp.src(`${config.src}/pages/*.hbs`)
       .pipe(handlebars({}, {
         ignorePartials: true,
         batch: [`${config.src}/partials`],
         helpers : {
-          valueWhenEqual : function(valA, valB, returnVal){
-            return valA === valB ? returnVal : '';
-          }
-        }
+          valueWhenEqual : (valA, valB, returnVal) => valA === valB ? returnVal : ''
+		}
       }))
       .pipe(rename({
         extname: '.html'
@@ -67,54 +36,64 @@ gulp.task('html', [], () => {
       .pipe(gulp.dest(config.dest));
 });
 
-gulp.task('ts', function () {
-  return gulp.src('src/**/*.ts')
-    .pipe(sourcemaps.init())
-    .pipe(ts({ module: 'es6' }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'));
+gulp.task('ts', () => {
+	return gulp.src('src/**/*.ts')
+		.pipe(sourcemaps.init())
+		.pipe(ts({module: 'es6'}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('less', function () {
-  return gulp.src(`${config.src}/**/main.less`)
-      .pipe(less())
-      .pipe(gulp.dest(config.dest));
+gulp.task('less', () => {
+	return gulp.src(`${config.src}/**/main.less`)
+		.pipe(less())
+		.pipe(gulp.dest(config.dest));
 });
 
-gulp.task('copy-assets', function () {
-  return gulp.src(`${config.src}/assets/**/*`)
-      .pipe(gulp.dest(`${config.dest}/assets`));
+gulp.task('copy-assets', () => {
+	return gulp.src(`${config.src}/assets/**/*`)
+		.pipe(gulp.dest(`${config.dest}/assets`));
 });
 
-gulp.task('copy-lib', function () {
-  return gulp.src(`${config.src}/lib/**/*`)
-      .pipe(gulp.dest(`${config.dest}/assets/lib`));
+gulp.task('copy-lib', () => {
+	return gulp.src(`${config.src}/lib/**/*`)
+		.pipe(gulp.dest(`${config.dest}/assets/lib`));
 });
 
-gulp.task('copy-sitemap', function () {
-  return gulp.src(`${config.src}/sitemap.xml`)
-      .pipe(gulp.dest(`${config.dest}`));
+gulp.task('copy-sitemap', () => {
+	return gulp.src(`${config.src}/sitemap.xml`)
+		.pipe(gulp.dest(`${config.dest}`));
 });
 
-gulp.task('serve', () => {
-  browserSync.init({
-    open: false,
-    notify: false,
-    files: [`${config.dest}/**/*`],
-    server: config.dest
-  });
+gulp.task('serve', done => {
+	browserSync.init({
+		open: false,
+		notify: false,
+		files: [`${config.dest}/**/*`],
+		server: config.dest
+	});
+	gulp.watch('./src/**/*.hbs', gulp.series('html'));
+	gulp.watch('./src/**/*.less', gulp.series('less'));
+	gulp.watch('./src/**/*.ts', gulp.series('ts'));
+	gulp.watch('./src/assets/**/*', gulp.series('copy-assets'));
+	gulp.watch('./gulpfile.js', gulp.parallel('html', 'less', 'ts', 'copy-assets', 'copy-lib', 'copy-sitemap'));
+	done();
 });
 
-gulp.task('watch', () => {
-  config.watchers.forEach(item => {
-    gulp.watch(item.match, item.tasks);
-  });
-});
+gulp.task('build', gulp.series('clean', gulp.parallel('html', 'less', 'ts', 'copy-assets', 'copy-lib', 'copy-sitemap', () => {
+	// noinspection JSUnusedGlobalSymbols
+	return gulp.src(`${config.src}/pages/*.hbs`)
+		.pipe(handlebars({}, {
+			ignorePartials: true,
+			batch: [`${config.src}/partials`],
+			helpers : {
+				valueWhenEqual : (valA, valB, returnVal) => valA === valB ? returnVal : ''
+			}
+		}))
+		.pipe(rename({ extname: '.html' }))
+		.pipe(gulp.dest(config.dest));
+})));
 
-gulp.task('default', ['build'], done => {
-  if (env.watch) {
-    gulp.start('serve');
-    gulp.start('watch');
-  }
-  done();
-});
+gulp.task('server', gulp.series('build', 'serve'));
+
+gulp.task('default', gulp.series('server'));
